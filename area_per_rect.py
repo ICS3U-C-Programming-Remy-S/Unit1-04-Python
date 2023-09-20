c2e36bbde4dbecbff7515c46a2b886a4845339c6
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-09-20 03:24:37 +0000

    Done area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created by: Remy Skelton
+# Date: Sep. 19,2023
+# This program calculates the are of a rectangle
+
+
+def main():
+    print("For a rectangle with a length of 13.4cm and width of 7.8cm:")
+    print()
+    print("The area is: {}cm^2".format(13.4 * 7.8))
+    print("The perimeter is: {}cm".format(2 * (13.4 + 7.8)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created by: Remy Skelton
+# Date: Sep. 19,2023
+# This program does basic math
+
+
+def main():
+    print("16-4=()".format(16 - 4))
+    print("20+15=()".format(20 + 15))
+    print("4*5=()".format(4 * 5))
+    print("12/6=()".format(12 / 6))
+    print("4/9=()".format(4 / 9))
+    print("4+17*2=()".format(4 + 17 * 2))
+    print("7^3=()".format(7 ^ 3))  # bitwise operator
+    print("3**6=()".format(3**6))  # exponent
+    print("25**0.5=()".format(25**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
